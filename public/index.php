d4a34ad7b501f80c7b30f70f79023a03e06d6367
JoseTaquiriVicerrel <vicerrel15@gmail.com>
2021-05-21 10:23:52 -0500

    Variables entorno - OK

--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
 <?php
 
+use Dotenv\Dotenv;
 use Libs\Core;
 
 require_once '../vendor/autoload.php';
-
-// require_once '../libs/controller.php';
-// require_once '../libs/core.php';
 require_once '../config/config.php';
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 $core = new Core();
-
-// echo "Hola mundo !!";
